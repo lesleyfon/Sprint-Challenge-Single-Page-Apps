ae832bb480bbff19498a5696ab19baccf0d798a0
lesley <lesleybanadzem2017@gmail.com>
2019-09-15 10:58:42 -0700

    completed the Tab to render each component when we are ont the right route

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,8 @@
 import React, { useEffect, useState } from "react";
 
-export default function LocationsList() {}
+export default function LocationsList() {
+
+    return (
+        <>LocationsList</>
+    )
+}
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Tab, Menu, Icon } from "semantic-ui-react";
-import { NavLink } from "react-router-dom";
+import { NavLink,Route } from "react-router-dom";
+import WelcomePage from "./WelcomePage";
+import CharacterList from "./CharacterList";
+import LocationsList from "./LocationsList";
 
 // TODO: Add missing menu/tabs/nav below
 
@@ -11,5 +14,20 @@ import { NavLink } from "react-router-dom";
 // https://react.semantic-ui.com/collections/breadcrumb/
 
 export default function TabNav() {
-
+    const panes = [
+        { menuItem: 
+             { key: 'home', icon: 'home', content: 'Home', },
+            render: () => <Route path='/' exact component = {WelcomePage} /> },
+        { menuItem: 
+             { key: 'character', icon: 'group', content: 'Character' },
+            render: () => <Route path='/character' exact component = {CharacterList} /> },
+        { menuItem:  
+            { key: 'location', icon: 'location arrow', content: 'Location' },
+            render: () =><Route path='/location' exact component = {LocationsList} />  },
+      ]
+    return (
+        <Tab 
+        panes={panes}
+       />
+    );
 };
