d82a172ace82436e13c40858c2bbd28f0183f5fb
lesley <lesleybanadzem2017@gmail.com>
2019-09-15 11:17:17 -0700

    Mad axios call to get character and used semantic ui to display each character on a card with some detail and Image of the character

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,39 @@
 import React, { useEffect, useState } from "react";
+import {Card, Image} from 'semantic-ui-react'
+import axios from "axios";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-
+  const [characters, setCharacter] = useState([])
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get('https://rickandmortyapi.com/api/character/')
+      .then(res=>{
+        setCharacter(res.data.results)
+      })
+      .catch(console.log)
   }, []);
 
   return (
     <section className="character-list grid-view">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {characters.map(character=>{
+        console.log(character)
+      return (       
+        <Card key={character.name}>
+            <Image src ={`${character.image}`}/>
+            <Card.Content>
+              <Card.Header>{character.name}</Card.Header>
+              <Card.Meta>{character.species}</Card.Meta>
+              <Card.Description>
+                Location: {character.location.name}
+              </Card.Description>
+              <Card.Description>
+                Origin: {character.origin.name}
+              </Card.Description>
+            </Card.Content>
+        </Card>  )
+      })}
     </section>
   );
 }
